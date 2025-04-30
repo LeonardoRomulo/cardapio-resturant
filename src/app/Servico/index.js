3481eec6bd1f2ebb8f3670dc72ab853f6eb2d1ae
LeonardoRomulo <leo.romulo15@gmail.com>
2025-04-30 15:18:08 -0300

    feat: adicionado a lógica para buscar pela descrição na função buscar produtos na camada de serviço

--- src/app/Servico/index.js
+++ src/app/Servico/index.js
@@ -11,6 +11,6 @@ export const buscarProdutos = (textoDigitado) => {
     return produtos.filter((produto) =>  
         produto.nome.toLowerCase().includes(textoDigitado.toLowerCase()) ||
         produto.categoria.toLowerCase().includes(textoDigitado.toLowerCase()) 
-        // || produto.descricao.toLowerCase().includes(textoDigitado.toLowerCase())
+        || produto.descricao.toLowerCase().includes(textoDigitado.toLowerCase())
 )
 }
